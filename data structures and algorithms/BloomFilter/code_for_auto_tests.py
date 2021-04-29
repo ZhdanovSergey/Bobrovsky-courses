e22305e7fd4170a6f95f8f38196fb1462de9ec0f
Sergey Zhdanov <sergeyk929@mail.ru>
2021-04-29 20:54:16 +0700

    bloomfilter template

---
+++ data structures and algorithms/BloomFilter/code_for_auto_tests.py
@@ -0,0 +1,27 @@
+class BloomFilter:
+
+	def __init__(self, f_len):
+		self.filter_len = f_len
+		# создаём битовый массив длиной f_len ...
+		pass
+
+
+	def hash1(self, str1):
+		# 17
+		for c in str1:
+			code = ord(c)
+		# реализация ...
+		pass
+
+	def hash2(self, str1):
+		# 223 
+		# ...
+		pass
+
+	def add(self, str1):
+		# добавляем строку str1 в фильтр
+		pass
+
+	def is_value(self, str1):
+		# проверка, имеется ли строка str1 в фильтре
+		pass
---
+++ data structures and algorithms/BloomFilter/my_tests.py
@@ -0,0 +1,12 @@
+import unittest
+from code_for_auto_tests import BloomFilter
+
+
+class BloomFilterTests(unittest.TestCase):
+
+	def test_(self):
+		print(0xffffffff)
+
+
+if __name__ == '__main__':
+	unittest.main()
