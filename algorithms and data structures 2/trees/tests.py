45306c3c5f449a80b627d34b6b8afbac3e0d60af
Sergey Zhdanov <sergeyk929@mail.ru>
2021-09-17 00:31:01 +0700

    ASD2 tree class and few unnecessary exercises

---
+++ algorithms and data structures 2/trees/tests.py
@@ -0,0 +1,26 @@
+import unittest
+from code import SimpleTree, SimpleTreeNode
+
+
+class SimpleTreeTests(unittest.TestCase):
+
+	# def _fill_binary_subtree(self, parent):
+	# 	node = SimpleTreeNode(self._current_value, parent)
+	# 	node.Children
+	# 	self._current_value =+ 1
+
+
+	# def generate_tree(self, nodes_number):
+	# 	tree = SimpleTree(SimpleTreeNode(0, None))
+	# 	self._current_value = 0
+
+	# def test_add_child(self):
+	# 	pass
+	# 	# bloom = BloomFilter(32)
+	# 	# bloom.add('0123456789')
+	# 	# self.assertTrue(bloom.is_value('0123456789'))
+	# 	# self.assertFalse(bloom.is_value('1234567890'))
+
+
+if __name__ == '__main__':
+	unittest.main()
