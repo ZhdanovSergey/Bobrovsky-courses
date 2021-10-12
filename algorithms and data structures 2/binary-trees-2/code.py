d28c17bb0d60cc784b084bdb2642137d2011d14a
Sergey Zhdanov <sergeyk929@mail.ru>
2021-10-12 19:55:46 +0700

    binary trees 2 - class stab

---
+++ algorithms and data structures 2/binary-trees-2/code.py
@@ -0,0 +1,15 @@
+class aBST:
+
+	def __init__(self, depth):
+		# правильно рассчитайте размер массива для дерева глубины depth:
+		tree_size = 0
+		self.Tree = [None] * tree_size # массив ключей
+	
+	def FindKeyIndex(self, key):
+		# ищем в массиве индекс ключа
+		return None # не найден
+	
+	def AddKey(self, key):
+		# добавляем ключ в массив
+		return -1; 
+		# индекс добавленного/существующего ключа или -1 если не удалось
