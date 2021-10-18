26ccfb60d330035b268994fd0d7b0efd189ec4fb
Sergey Zhdanov <sergeyk929@mail.ru>
2021-10-19 01:11:48 +0700

    binary trees 2 - array binary tree methods with tests

--- algorithms and data structures 2/binary-trees-2/code.py
+++ algorithms and data structures 2/binary-trees-2/code.py
@@ -2,14 +2,40 @@ class aBST:
 
 	def __init__(self, depth):
 		# правильно рассчитайте размер массива для дерева глубины depth:
-		tree_size = 0
+		tree_size = 2 ** depth - 1
 		self.Tree = [None] * tree_size # массив ключей
 	
 	def FindKeyIndex(self, key):
 		# ищем в массиве индекс ключа
-		return None # не найден
+		def findKeyRecursive(key, current_index):
+			if self.Tree[current_index] == None:
+				return -current_index
+			if self.Tree[current_index] == key:
+				return current_index
+
+			leftChildIndex = current_index * 2 + 1
+			rightChildIndex = current_index * 2 + 2
+
+			if key < self.Tree[current_index] and leftChildIndex < len(self.Tree):
+				return findKeyRecursive(key, leftChildIndex)
+			if key > self.Tree[current_index] and rightChildIndex < len(self.Tree):
+				return findKeyRecursive(key, rightChildIndex)
+
+			return None
+
+		if len(self.Tree) > 0:
+			return findKeyRecursive(key, 0)
+		return None
 	
 	def AddKey(self, key):
 		# добавляем ключ в массив
-		return -1; 
+		findResult = self.FindKeyIndex(key)
+
+		if findResult is None or \
+			findResult > 0 or \
+			findResult == 0 and self.Tree[0] is not None:
+			return -1
+
+		self.Tree[-findResult] = key
+		return -findResult
 		# индекс добавленного/существующего ключа или -1 если не удалось
---
+++ algorithms and data structures 2/binary-trees-2/tests.py
@@ -0,0 +1,103 @@
+import unittest
+import random
+from code import aBST
+
+
+class aBSTTests(unittest.TestCase):
+	def getTestTree(self):
+		tree = aBST(4)
+		tree.Tree = [50, 25, 75, None, 37, 62, 84, None, None, 31, 43, 55, None, None, 92]
+		return tree
+
+	def test_FindKeyIndexEmptyTree(self):
+		tree = aBST(0)
+		result = tree.FindKeyIndex(1)
+		self.assertTrue(result is None)
+
+	def test_FindKeyIndexOneLevelSuccess(self):
+		tree = aBST(1)
+		testKey = 1
+		tree.Tree[0] = testKey
+		result = tree.FindKeyIndex(testKey)
+		self.assertEqual(result, 0)
+
+	def test_FindKeyIndexOneLevelEmpty(self):
+		tree = aBST(1)
+		result = tree.FindKeyIndex(1)
+		self.assertEqual(result, 0)
+
+	def test_FindKeyIndexOneLevelUnsuccess(self):
+		tree = aBST(1)
+		tree.Tree[0] = 1
+		result = tree.FindKeyIndex(2)
+		self.assertTrue(result is None)
+
+	def test_FindKeyIndexSuccess(self):
+		tree = self.getTestTree()
+		result = tree.FindKeyIndex(62)
+		self.assertEqual(result, 5)
+
+	def test_FindKeyIndexSuccessRoot(self):
+		tree = self.getTestTree()
+		result = tree.FindKeyIndex(50)
+		self.assertEqual(result, 0)
+
+	def test_FindKeyIndexSuccessLeaf(self):
+		tree = self.getTestTree()
+		result = tree.FindKeyIndex(43)
+		self.assertEqual(result, 10)
+
+	def test_FindKeyIndexEmptyCell(self):
+		tree = self.getTestTree()
+		result = tree.FindKeyIndex(24)
+		self.assertEqual(result, -3)
+
+	def test_FindKeyIndexOutOfTree(self):
+		tree = self.getTestTree()
+		result = tree.FindKeyIndex(54)
+		self.assertEqual(result, None)
+
+	def test_AddKeyEmptyTree(self):
+		tree = aBST(0)
+		result = tree.AddKey(1)
+		self.assertEqual(result, -1)
+
+	def test_AddKeyOneLevelSuccess(self):
+		tree = aBST(1)
+		result = tree.AddKey(1)
+		self.assertEqual(result, 0)
+
+	def test_AddKeyOneLevelAlreadyExist(self):
+		tree = aBST(1)
+		tree.Tree[0] = 0
+		result = tree.AddKey(0)
+		self.assertEqual(result, -1)
+
+	def test_AddKeyOneLevelOutOfTree(self):
+		tree = aBST(1)
+		tree.Tree[0] = 0
+		result = tree.AddKey(1)
+		self.assertEqual(result, -1)
+
+	def test_AddKeySuccess(self):
+		tree = self.getTestTree()
+		result = tree.AddKey(24)
+		self.assertEqual(result, 3)
+
+	def test_AddKeyAlreadyExist(self):
+		tree = self.getTestTree()
+		result = tree.AddKey(62)
+		self.assertEqual(result, -1)
+
+	def test_AddKeyAlreadyExistLeaf(self):
+		tree = self.getTestTree()
+		result = tree.AddKey(92)
+		self.assertEqual(result, -1)
+
+	def test_AddKeyOutOfTree(self):
+		tree = self.getTestTree()
+		result = tree.AddKey(45)
+		self.assertEqual(result, -1)
+
+if __name__ == '__main__':
+	unittest.main()
