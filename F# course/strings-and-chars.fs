fb230932161308b45ec90c404905a8e4163fa912
ZhdanovSergey <37261841+ZhdanovSergey@users.noreply.github.com>
2023-04-28 11:32:02 +0300

    F#(strings-and-chars): handle out-of-range inputs

--- F# course/strings-and-chars.fs
+++ F# course/strings-and-chars.fs
@@ -1,13 +1,16 @@
 // 17.1
 let rec pow = function
-  | (s,1) -> s
+  | (s,n) when n <= 0 -> ""
   | (s,n) -> s + pow(s,n-1)
   
 // 17.2
-let rec isIthChar = fun (s: string, n: int ,c: char) -> s.[n] = c
+let rec isIthChar = function
+  | (s,n,c) when n < 0 || n >= (String.length s) -> false
+  | (s,n,c) -> s.[n] = c
 
 // 17.3
 let rec occFromIth = function
-  | (s,n,c) when (String.length s) = n -> 0
+  | (s,n,c) when n >= (String.length s) -> 0
+  | (s,n,c) when n < 0 -> occFromIth(s,0,c)
   | (s,n,c) when s.[n] = c -> 1 + occFromIth(s,n+1,c)
   | (s,n,c) when not (s.[n] = c) -> occFromIth(s,n+1,c)
