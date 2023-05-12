e7750c2c7fb9d84cbf24b714953480bd24699808
ZhdanovSergey <37261841+ZhdanovSergey@users.noreply.github.com>
2023-05-12 19:22:51 +0700

    F#: fix evenn

--- F# course/lists.fs
+++ F# course/lists.fs
@@ -15,6 +15,6 @@ let dnto n =
 // 34.3
 let evenn n =
   let rec evennRec = function
-    | (k, acc) when k <= 0 -> acc
+    | (k, acc) when k < 0 -> acc
     | (k, acc) -> evennRec(k-2, k :: acc)
-  evennRec(n*2, [])
+  evennRec((n-1)*2, [])
