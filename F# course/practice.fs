4910ef892ba48e4957d2512ec852bad8a1bc8ede
ZhdanovSergey <37261841+ZhdanovSergey@users.noreply.github.com>
2023-05-18 23:10:42 +0700

    F#: practice without realization

---
+++ F# course/practice.fs
@@ -0,0 +1,29 @@
+// 40.1
+let rec sum (p, xs) = 0
+
+// 40.2.1
+let rec count (xs, n) = 0
+
+// 40.2.2
+let rec insert (xs, n) = []
+
+// 40.2.3
+let rec intersect (xs1, xs2) = []
+
+// 40.2.4
+let rec plus (xs1, xs2) = []
+
+// 40.2.5
+let rec minus (xs1, xs2) = []
+
+// 40.3.1
+let rec smallest = Some(0)
+
+// 40.3.2
+let rec delete (n, xs) = []
+
+// 40.3.3
+let rec sort = []
+
+// 40.4
+let rec revrev = []
