9d201851ae48f967c5ea1895d54a4ac07a52a7fa
ZhdanovSergey <37261841+ZhdanovSergey@users.noreply.github.com>
2023-05-30 22:36:16 +0700

    F#: collections №2,3

--- F# course/collections.fs
+++ F# course/collections.fs
@@ -2,3 +2,14 @@
 let list_filter f xs =
   let filter x acc = if (f x) then x :: acc else acc
   List.foldBack filter xs []
+
+// 41.4.2
+let sum (p, xs) =
+  let sum_step acc x = if (p x) then x + acc else acc
+  List.fold sum_step 0 xs
+  
+// 41.4.3
+let revrev xs =
+  let rev_step acc x = x :: acc
+  let revrev_step acc x = (List.fold rev_step [] x) :: acc
+  List.fold revrev_step [] xs
