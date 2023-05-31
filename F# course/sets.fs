70f45e16053d8a49daeb7263afeca7c3d7eb22a4
ZhdanovSergey <37261841+ZhdanovSergey@users.noreply.github.com>
2023-05-31 12:55:16 +0300

    F#: fix sets

--- F# course/sets.fs
+++ F# course/sets.fs
@@ -1,6 +1,7 @@
 // 42.3
 let rec allSubsets n k = match (n, k) with
-  | (n, k) when n < k || k = 0 -> Set.empty
-  | (n, k) when n >= k -> Set.union (allSubsets (n - 1) k) (Set.map (fun x -> Set.add n x) (allSubsets (n - 1) (k - 1)))
-  
-printfn "%A" (allSubsets 0 0)
+  | (n, k) when n < k -> Set.empty
+  | (n, 0) -> set [Set.empty]
+  | (n, k) ->
+    let add_n subset = Set.add n subset
+    Set.union (allSubsets (n - 1) k) (Set.map add_n (allSubsets (n - 1) (k - 1)))
