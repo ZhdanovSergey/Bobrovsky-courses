0c35d1339b7125cd709b926c801a39ca09104a9a
ZhdanovSergey <37261841+ZhdanovSergey@users.noreply.github.com>
2023-06-06 07:38:24 +0300

    F#: lazy, bigList variant

--- F# course/lazy.fs
+++ F# course/lazy.fs
@@ -3,3 +3,8 @@ let rec fibo1 n n1 n2 = match n with
   | 0 -> n2
   | 1 -> n1
   | n -> fibo1 (n - 1) (n1 + n2) n1
+
+// 48.4.3
+let rec bigList n k =
+  if n=0 then k []
+  else bigList (n-1) (fun res -> k(1::res))
