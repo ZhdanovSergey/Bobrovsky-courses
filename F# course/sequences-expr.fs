38a6b432fef53cb8184eb5cad71c6a02ea14810c
ZhdanovSergey <37261841+ZhdanovSergey@users.noreply.github.com>
2023-06-07 13:45:50 +0300

    F#: sequences-expr, improved reference solution

--- F# course/sequences-expr.fs
+++ F# course/sequences-expr.fs
@@ -1,18 +1,22 @@
 // 50.2.1
-let rec fac n res = seq { 
-     yield res 
-     yield! fac (n + 1) (res * n) } 
-  
-let fac_seq = seq { 
-     yield 1 
-     yield! seq (fac 2 1) } 
+let rec fac n res = seq {
+  yield res
+  yield! fac (n + 1) (res * (n + 1))
+}
+
+let fac_seq = seq {
+  yield 1
+  yield! seq (fac 1 1)
+}
 
 // 50.2.2
-let rec f i= seq { 
-     yield (0 - i) 
-     yield i 
-     yield! f(i + 1) } 
+let rec f i = seq {
+  yield (0 - i)
+  yield i
+  yield! f(i + 1)
+}
   
-let seq_seq = seq { 
-     yield 0 
-     yield! f 1 }
+let seq_seq = seq {
+  yield 0
+  yield! f 1
+}
