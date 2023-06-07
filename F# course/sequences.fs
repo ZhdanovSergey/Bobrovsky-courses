e65a3824bbeac3f10966e748f04143720bd9b306
ZhdanovSergey <37261841+ZhdanovSergey@users.noreply.github.com>
2023-06-07 06:27:28 +0300

    F#: sequences

---
+++ F# course/sequences.fs
@@ -0,0 +1,8 @@
+// 49.5.1
+let even_seq = Seq.initInfinite (fun n -> 2 * n)
+
+// 49.5.2
+let fac_seq = Seq.initInfinite (fun n -> List.fold (*) 1 [1..n])
+
+// 49.5.3
+let seq_seq = Seq.initInfinite (fun n -> if n % 2 = 0 then (n+1)/2 else -(n+1)/2)
