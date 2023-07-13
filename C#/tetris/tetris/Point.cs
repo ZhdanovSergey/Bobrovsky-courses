231d21544cae7a27d65c2e02681ef9d80e0af28d
Sergey Zhdanov <sergeyk929@mail.ru>
2023-07-13 09:19:13 +0700

    С#: tetris

---
+++ C#/tetris/tetris/Point.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris
+{
+    internal class Point
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public char C { get; set; }
+
+        public Point(int x, int y, char c)
+        {
+            this.X = x;
+            this.Y = y;
+            this.C = c;
+        }
+
+        public Point(Point p)
+        {
+            this.X = p.X;
+            this.Y = p.Y;
+            this.C = p.C;
+        }
+        public void Draw()
+        {
+            Console.SetCursorPosition(X, Y);
+            Console.Write(C);
+            Console.SetCursorPosition(0, 0);
+        }
+
+        public void Hide()
+        {
+            Console.SetCursorPosition(X, Y);
+            Console.Write(" ");
+        }
+
+        public void Move(Direction dir)
+        {
+            switch (dir)
+            {
+                case Direction.LEFT:
+                    this.X -= 1; break;
+
+                case Direction.RIGHT:
+                    this.X += 1; break;
+
+                case Direction.DOWN:
+                    this.Y += 1; break;
+            }
+        }
+    }
+}
---
+++ C#/tetris/tetris/Stick.cs
@@ -0,0 +1,46 @@
+﻿namespace Tetris
+{
+    internal class Stick : Figure
+    {
+        public Stick(int x, int y, char sym)
+        {
+            this.Points[0] = new Point(x, y, sym);
+            this.Points[1] = new Point(x, y + 1, sym);
+            this.Points[2] = new Point(x, y + 2, sym);
+            this.Points[3] = new Point(x, y + 3, sym);
+
+            Draw();
+        }
+
+        public override void Rotate(Point[] points)
+        {
+            if (points[0].X == points[1].X)
+            {
+                RotateHorizontal(points);
+            }
+            else
+            {
+                RotateVertical(points);
+            }
+        }
+
+        static void RotateHorizontal(Point[] points)
+        {
+            for (int i = 0; i < points.Length; i++)
+            {
+                points[i].X = points[0].X + i;
+                points[i].Y = points[0].Y;
+
+            }
+        }
+
+        static void RotateVertical(Point[] points)
+        {
+            for (int i = 0; i < points.Length; i++)
+            {
+                points[i].X = points[0].X;
+                points[i].Y = points[0].Y + i;
+            }
+        }
+    }
+}
