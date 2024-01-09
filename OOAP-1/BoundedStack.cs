21f43dbc1188c7750a7197a87e281c26f9b65e38
ZhdanovSergey <sergeyk929@mail.ru>
2024-01-09 21:52:45 +0700

    OOAP-1: init project and make BoundedStack

---
+++ OOAP-1/BoundedStack.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOAP_1;
+
+internal class BoundedStack<T>
+{
+    private List<T> stack = new();
+
+    public const int PEEK_NIL = 0;
+    public const int PEEK_OK = 1;
+    public const int PEEK_ERR = 2;
+
+    public const int POP_NIL = 0;
+    public const int POP_OK = 1;
+    public const int POP_ERR = 2;
+
+    public const int PUSH_NIL = 0;
+    public const int PUSH_OK = 1;
+    public const int PUSH_ERR = 2;
+
+    public int MaxCapacity { get; init; }
+    public int Size { get => stack.Count; }
+    public int PeekStatus { get; private set; } = PEEK_NIL;
+    public int PopStatus { get; private set; } = POP_NIL;
+    public int PushStatus { get; private set; } = PUSH_NIL;
+
+    public BoundedStack(int maxCapacity = 32)
+    {
+        MaxCapacity = maxCapacity;
+    }
+    public void Clear()
+    {
+        stack = new();
+        PeekStatus = PEEK_NIL;
+        PopStatus = POP_NIL;
+        PushStatus = PUSH_NIL;
+    }
+    public T? Peek()
+    {
+        T? result = default;
+
+        if (Size > 0)
+        {
+            result = stack[^1];
+            PeekStatus = PEEK_OK;
+        }
+        else
+        {
+            PeekStatus = PEEK_ERR;
+        }
+
+        return result;
+    }
+    public void Pop()
+    {
+        if (Size > 0)
+        {
+            stack.RemoveAt(Size - 1);
+            PopStatus = POP_OK;
+        }
+        else
+        {
+            PopStatus = POP_ERR;
+        }
+    }
+    public void Push(T value)
+    {
+        if (Size < MaxCapacity)
+        {
+            stack.Add(value);
+            PushStatus = PUSH_OK;
+        } else
+        {
+            PushStatus = PUSH_ERR;
+        }
+    }
+}
---
+++ OOAP-1/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace OOAP_1;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
