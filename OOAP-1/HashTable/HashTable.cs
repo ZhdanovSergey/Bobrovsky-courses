57d0bfab5641686fb13a3d1838e3a0f8daee4959
ZhdanovSergey <sergeyk929@mail.ru>
2024-02-12 23:37:18 +0700

    OOAP-1: NativeDictionary reflection, PowerSet based on HashTable implementation

--- OOAP-1/HashTable/HashTable.cs
+++ OOAP-1/HashTable/HashTable.cs
@@ -1,19 +1,22 @@
 ﻿using OOAP_1.TwoWayList;
 using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace OOAP_1.HashTable;
 
-interface IHashTable<TValue>
+interface IHashTable<TValue> : IEnumerable<TValue>
 {
     // КОНСТРУКТОР
     abstract static IHashTable<TValue> Create(int maxSize);
 
     // КОМАНДЫ
-    // постусловие: если не возникло неразрешимых коллизий, в таблицу добавлено значение
+    // предусловие: в таблице есть свободный слот
+    // постусловие: в таблицу добавлено значение
     void Add(TValue value);
 
     // предусловие: в таблице существует переданное значение
@@ -23,6 +26,7 @@ interface IHashTable<TValue>
     // ЗАПРОСЫ
     bool Contains(TValue value);
     int MaxSize { get; }
+    int Size { get; }
 
     // ДОПОЛНИТЕЛЬНЫЕ ЗАПРОСЫ
     int AddStatus { get; }
@@ -32,7 +36,7 @@ interface IHashTable<TValue>
 class HashTable<TValue> : IHashTable<TValue>
 {
     public const int STEP = 3;
-    readonly TValue?[] slots;
+    protected TValue[] slots;
 
     public const int ADD_NIL = 0; // Add() ещё не вызывалась
     public const int ADD_OK = 1; // последняя Add() отработала нормально
@@ -53,6 +57,7 @@ class HashTable<TValue> : IHashTable<TValue>
             if (slots[index].Equals(default(TValue)))
             {
                 slots[index] = value;
+                Size++;
                 AddStatus = ADD_OK;
                 return;
             }
@@ -67,6 +72,7 @@ class HashTable<TValue> : IHashTable<TValue>
             if (slots[index].Equals(value))
             {
                 slots[index] = default(TValue);
+                Size--;
                 RemoveStatus = REMOVE_OK;
                 return;
             }
@@ -76,24 +82,26 @@ class HashTable<TValue> : IHashTable<TValue>
     }
 
     // ЗАПРОСЫ
+    public int Size { get; protected set; } = 0;
     public int MaxSize { get => slots.Length; }
 
     public bool Contains(TValue value) =>
-        GetSlotsIterator(value)
-            .Select(index => slots[index])
-            .Contains(value);
+        GetSlotsIterator(value).Select(index => slots[index]).Contains(value);
 
     // ДОПОЛНИТЕЛЬНЫЕ ЗАПРОСЫ
-    public int AddStatus { get; private set; } = ADD_NIL;
-    public int RemoveStatus { get; private set; } = REMOVE_NIL;
+    public int AddStatus { get; protected set; } = ADD_NIL;
+    public int RemoveStatus { get; protected set; } = REMOVE_NIL;
+    public IEnumerator<TValue> GetEnumerator() => slots
+        .Where(value => !value.Equals(default(TValue))).GetEnumerator();
 
-    HashTable(int maxSize)
+    protected HashTable(int maxSize)
     {
         if (maxSize % STEP == 0)
             throw new Exception($"{nameof(maxSize)} should not be divisible by {STEP}");
 
         slots = new TValue[maxSize];
     }
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
 
     IEnumerable<int> GetSlotsIterator(TValue value)
     {
---
+++ OOAP-1/PowerSet/PowerSet.cs
@@ -0,0 +1,90 @@
+﻿using OOAP_1.HashTable;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOAP_1.PowerSet;
+
+interface IPowerSet<TValue> : IHashTable<TValue>
+{
+    // КОНСТРУКТОР
+    // постусловие: создано пустое множество
+    new abstract static IPowerSet<TValue> Create(int maxSize);
+
+    // КОМАНДЫ
+    // предусловие: в множестве нет переданного значения и есть свободный слот
+    // постусловие: переданное значение добавлено в множество
+    // void Add(TValue value);
+
+    // ЗАПРОСЫ
+    IPowerSet<TValue> Intersection(IPowerSet<TValue> powerSet);
+    IPowerSet<TValue> Union(IPowerSet<TValue> powerSet);
+    IPowerSet<TValue> Difference(IPowerSet<TValue> powerSet);
+    bool IsSubset(IPowerSet<TValue> powerSet);
+}
+
+class PowerSet<TValue> : HashTable<TValue>, IPowerSet<TValue>
+{
+    public const int ADD_ERR_DUPLICATE = 3; // переданное значение уже есть в множестве
+    // КОНСТРУКТОР
+    public new static IPowerSet<TValue> Create(int maxSize) => new PowerSet<TValue>(maxSize);
+
+    // КОМАНДЫ
+    public new void Add(TValue value)
+    {
+        if (this.Contains(value))
+        {
+            AddStatus = ADD_ERR_DUPLICATE;
+            return;
+        }
+
+        base.Add(value);
+    }
+
+    // ЗАПРОСЫ
+    public IPowerSet<TValue> Intersection(IPowerSet<TValue> paramSet)
+    {
+        var result = new PowerSet<TValue>(Math.Min(MaxSize, paramSet.MaxSize));
+
+        foreach (var thisValue in this)
+            if (paramSet.Contains(thisValue))
+                result.Add(thisValue);
+
+        return result;
+    }
+    public IPowerSet<TValue> Union(IPowerSet<TValue> paramSet)
+    {
+        var result = new PowerSet<TValue>(Math.Max(MaxSize, paramSet.MaxSize));
+
+        foreach (var thisValue in this)
+            result.Add(thisValue);
+
+        foreach (var paramValue in paramSet)
+            result.Add(paramValue);
+
+        return result;
+    }
+    public IPowerSet<TValue> Difference(IPowerSet<TValue> paramSet)
+    {
+        var result = new PowerSet<TValue>(MaxSize);
+
+        foreach (var thisValue in this)
+            if (!paramSet.Contains(thisValue))
+                result.Add(thisValue);
+
+        return result;
+    }
+
+    public bool IsSubset(IPowerSet<TValue> paramSet)
+    {
+        foreach (var paramValue in paramSet)
+            if (!this.Contains(paramValue))
+                return false;
+
+        return true;
+    }
+
+    PowerSet(int maxSize) : base(maxSize) {}
+}
