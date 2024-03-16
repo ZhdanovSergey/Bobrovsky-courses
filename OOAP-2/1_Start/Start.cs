14b07cab47c56357995795303615209166e5a61b
ZhdanovSergey <sergeyk929@mail.ru>
2024-03-16 18:37:46 +0700

    OOAP-2: first exercise

---
+++ OOAP-2/1_Start/Start.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOAP_2._1_Start;
+
+class Person
+{
+    public required string Name { get; init; }
+
+    public virtual void PrintInfo() =>
+        Console.WriteLine($"Name: {Name}");
+}
+
+// Пример наследования: класс Employee содержит унаследованное от Person свойство Name
+class Employee : Person
+{
+    public required string Profession { get; init; }
+    public override void PrintInfo() =>
+        Console.WriteLine($"Name: {Name}, Profession: {Profession}");
+}
+
+// Пример композиции: класс Company содержит Employee как одно из полей
+class Company
+{
+    public readonly Employee BestEmployee = new Employee { Name = "Sergey", Profession = "Programmer" };
+}
+
+// Пример полиморфизма: можно использовать экземпляр Employee как экземпляр Person
+class PolimorficExample
+{
+    public void Main()
+    {
+        Person person = new Employee { Name = "Sergey", Profession = "Programmer" };
+        person.PrintInfo();
+    }
+}
---
+++ OOAP-2/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace OOAP_2;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
