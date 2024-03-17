bae1f8ce90d88bca8b523dc2d42cf83051247789
ZhdanovSergey <sergeyk929@mail.ru>
2024-03-18 06:45:09 +0700

    OOAP-2: exercise 2

---
+++ OOAP-2/2/Code.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOAP_2._2;
+
+// Я никогда ранее не слышал про наследование как более общий случай класса-родителя,
+// в моем понимании такое наследование противоречило бы отношению "is-a".
+
+class Person
+{
+    public required string Name { get; init; }
+}
+
+// уточнение класса Person
+class Employee : Person
+{
+    public required string Profession { get; init; }
+}
