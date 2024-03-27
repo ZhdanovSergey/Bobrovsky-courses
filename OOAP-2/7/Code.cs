7a91c8eef36d64ce07d6678af8f3492e843ded44
ZhdanovSergey <sergeyk929@mail.ru>
2024-03-27 13:52:54 +0700

    OOAP-2: exercise 7

---
+++ OOAP-2/7/Code.cs
@@ -0,0 +1,34 @@
+﻿// По тексту урока я вообще не понял что такое динамическое связываение. Судя по тому, что пишут в интернете, динамическое связывание можно представить так:
+
+abstract class Animal
+{
+    public virtual void MakeSound() =>
+        Console.WriteLine("Some sound");
+}
+
+class Dog : Animal
+{
+    public override void MakeSound() =>
+        Console.WriteLine("Bark");
+}
+
+class Cat : Animal
+{
+    public override void MakeSound() =>
+        Console.WriteLine("Meow");
+}
+
+class Program
+{
+    static void Main()
+    {
+        Animal myAnimal;
+
+        // Динамическое связывание - решение о том, какой именно метод MakeSound вызывать, принимается во время выполнения программы
+        myAnimal = new Dog();
+        myAnimal.MakeSound(); // Вывод: Bark
+
+        myAnimal = new Cat();
+        myAnimal.MakeSound(); // Вывод: Meow
+    }
+}
