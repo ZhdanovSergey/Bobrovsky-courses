04fa627a746c984c2a1aecc5ae42d47c1092e109
ZhdanovSergey <sergeyk929@mail.ru>
2024-04-06 19:30:59 +0700

    ooap-2: exercise 9

---
+++ OOAP-2/9/Code.cs
@@ -0,0 +1,62 @@
+﻿using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text.Json;
+
+abstract class Any : General {}
+
+abstract class General
+{
+    public void ShallowCopyTo<TTarget>(ref TTarget target)
+    {
+        target = (TTarget) MemberwiseClone();
+    }
+
+    public void DeepCopyTo<TTarget>(ref TTarget target)
+    {
+        target = DeepClone<TTarget>();
+    }
+
+    public TResult DeepClone<TResult>()
+    {
+        using var stream = ToMemoryStream();
+        var formatter = new BinaryFormatter();
+
+        return (TResult) formatter.Deserialize(stream);
+    }
+
+    public bool Equals(General obj) => base.Equals(obj);
+
+    public bool DeepEquals(General obj)
+    {
+        using var thisStream = ToMemoryStream();
+        using var objStream = obj.ToMemoryStream();
+
+        if (thisStream.Length != objStream.Length)
+            return false;
+
+        var thisByteArray = thisStream.ToArray();
+        var objByteArray = objStream.ToArray();
+
+        return thisByteArray.SequenceEqual(objByteArray);
+    }
+
+    public string Serialize() => JsonSerializer.Serialize(this);
+
+    public TResult? Deserialize<TResult>(string json) => JsonSerializer.Deserialize<TResult>(json);
+
+    public string Print() => Serialize();
+
+    public bool CheckType(Type type) => GetType() == type;
+    public new Type GetType() => base.GetType();
+
+    private MemoryStream ToMemoryStream()
+    {
+        var formatter = new BinaryFormatter();
+        var stream = new MemoryStream();
+
+        formatter.Serialize(stream, this);
+        stream.Position = 0;
+
+        return stream;
+    }
+}
