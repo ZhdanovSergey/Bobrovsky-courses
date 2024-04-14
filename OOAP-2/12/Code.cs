b1248dfbfeab3003891e3d2615561d6310a09eb9
ZhdanovSergey <sergeyk929@mail.ru>
2024-04-14 11:18:27 +0700

    OOAP-2: exercise 12

---
+++ OOAP-2/12/Code.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text.Json;
+
+namespace OOAP_2._12;
+
+abstract class General
+{
+    public void AssignmentAttempt<TTarget>(ref TTarget? target) where TTarget : class
+    {
+        target = this as TTarget; // присваивает null в случае неудачи
+    }
+}
+
+abstract class Any : General
+{
+    // наследует AssignmentAttempt
+}
